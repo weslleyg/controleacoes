d20611902da71cad2e2aa9d7f79e046f917a5e24
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-01-20 18:32:11 -0300

    utilizando mongodb

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'dg72736fd4-f3674-f346fg65-3hf4-gh69h-37ftgj6sk3';
+global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+    connectionString: 'mongodb+srv://projdarf:q1w2e3@cluster0.nfurv.mongodb.net/cluster0?retryWrites=true&w=majority',
+    sendgridKey: 'TBD',
+    conetainerConnectionString: 'TBD'
+};
---
+++ src/models/trade.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Trade = new mongoose.Schema({
+    date: {
+
+    },
+    time: {
+
+    },
+    type: {
+
+    },
+    ticket: {
+
+    },
+    entryPrice: {
+
+    },
+    balance: {
+
+    },
+    nShares: {
+
+    },
+    stopLoss: {
+
+    },
+    takeProfit: {
+
+    },
+    ratio: {
+
+    },
+    exitPrice: {
+
+    },
+    tradeCosts: {
+
+    },
+    note: {
+
+    }
+});
+
