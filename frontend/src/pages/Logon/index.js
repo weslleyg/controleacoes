ae2200f383f162c386afae2a884688b862a87565
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-03-01 17:15:09 -0300

    pagina de login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import "./styles.css";
+import { FiLogIn } from 'react-icons/fi';
 
 export default function Logon() {
     return (
-        <h1>Teste</h1>
+        <div className="logon-container">
+            <section className="form">
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua ID" />
+                    <button className="button" type="submit">Entrar</button>
+
+                    <a href='/register'>
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+        </div>
     );
-}
+};
